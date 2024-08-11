eae0f092fd5205178d3f07a196a6181fa1687fdf
Michael Hurgin <mixah@camtek.com>
2024-08-11 17:18:07 +0300

    First

---
+++ Source/main.ts
@@ -0,0 +1,40 @@
+interface IUser
+{
+    readonly firstName : string
+    readonly secondName : string        
+
+    toTextString(): void;
+}
+
+class User implements IUser
+{
+    constructor(firstName : string, secondName : string)
+    {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.count = 42;
+    }
+
+    private count : number
+
+    public firstName: string;
+    public secondName: string;
+
+    public toTextString(): string {
+        return "'" + this.firstName + "' '" + this.secondName + "' " + this.count
+    }
+}
+
+console.log("Mixa1")
+
+let user1 : IUser = { 
+    firstName : 'User11 Name', 
+    secondName : 'User1 Second Name', 
+    toTextString: ()=> user1.firstName 
+}
+
+
+let user2 : IUser =  new User('User2 Name', 'User2 Second Name');
+
+console.log(user1.toTextString())
+console.log(user2.toTextString())
